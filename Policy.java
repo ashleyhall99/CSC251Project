3fc194de543b8a3e2399f4e64a9c562c846a40dc
Ashley Hall <59103743+ashleyhall99@users.noreply.github.com>
2023-10-23 21:41:26 -0400

    Moved fields and methods
    Moved fields and methods to the PolicyHolder

--- Policy.java
+++ Policy.java
@@ -6,12 +6,7 @@ public class Policy {
     
     private int policyNumber;
     private String providerName;
-    private String firstName;
-    private String lastName;
-    private int age;
-    private String smokingStatus;
-    private double height;
-    private double weight;
+   
     
     
    /**
@@ -83,87 +78,7 @@ public class Policy {
      * Sets the first name of the user
      * @param fN - first name
      */
-   public void setfirstName(String fN){
-        firstName = fN;
-    }
-    /**
-     * accessor method for firstName
-     * @return firstName
-     */
-      public String getfirstName(){
-        return firstName;
-    }
-    /**
-     * sets the last name of the user
-     * @param lN - last name
-     */
-   public void setlastName(String lN){
-        lastName = lN;
-    }
-    /**
-     * accessor method for lastName
-     * @return lastName
-     */
-      public String getlastName(){
-        return lastName;
-    }
-   /**
-    * sets the age of the user
-    * @param a - age
-    */
-   public void setAge(int a){
-        age = a;
-    }
-    /**
-     * accessor method for age
-     * @return users age
-     */
-      public int getAge(){
-        return age;
-    }
-   /**
-    * sets the smoking status of the user
-    * @param sS - smoking status
-    */
-   public void setsmokingStatus(String sS){
-        smokingStatus = sS;
-    }
-    /**
-     * accessor method for smokingStatus
-     * @return smoking status 
-     */
-      public String getsmokingStatus(){
-        return smokingStatus;
-    }
-   /**
-    * sets the height for the user
-    * @param h - height
-    */
-   public void setHeight(double h){
-        height = h;
-    }
-    /**
-     * accessor method for height
-     * @return users height
-     */
-      public double getHeight(){
-        return height;
-    }
-   /**
-    * sets the weight for the user
-    * @param w - weight
-    */
-   public void setWeight(double w){
-        weight = w;
-    }
-    /**
-     * accessor method for weight
-     * @return users weight
-     */
-      public double getWeight(){
-        return weight;
-    }
-      /**
+        /**
        * A method to get BMI for policyholder
        * @return - BMI for user
        */
